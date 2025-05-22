7f406eb6b5560d314463d6251e02441cc0335452
dkocheshkov <d.kocheshkov@sel-be.ru>
2025-05-22 23:29:28 +0300

    Обновляем dto

--- src/main/java/ru/practicum/onlinestore/dto/response/ItemDTO.java
+++ src/main/java/ru/practicum/onlinestore/dto/response/ItemDTO.java
@@ -2,4 +2,4 @@ package ru.practicum.onlinestore.dto.response;
 
 import java.math.BigDecimal;
 
-public record ItemDTO(Long id, String title, String description, BigDecimal price, BigDecimal count, Long imageId) {}
+public record ItemDTO(Long id, String title, String description, BigDecimal price, BigDecimal count, String imageId) {}
