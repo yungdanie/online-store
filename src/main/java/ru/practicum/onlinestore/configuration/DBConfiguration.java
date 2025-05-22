6d60ac1f33918bc0130af34946aefc9ffe625a28
dkocheshkov <d.kocheshkov@sel-be.ru>
2025-05-22 23:32:33 +0300

    Добавляем/удаляем конфигурации

--- src/main/java/ru/practicum/onlinestore/configuration/DBConfiguration.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.onlinestore.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-@Configuration
-@EnableTransactionManagement(proxyTargetClass = true)
-public class DBConfiguration {}
--- src/main/java/ru/practicum/onlinestore/configuration/MainConfiguration.java
+++ src/main/java/ru/practicum/onlinestore/configuration/MainConfiguration.java
@@ -1,16 +1,6 @@
 package ru.practicum.onlinestore.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 
 @Configuration
-public class MainConfiguration {
-
-    @Bean
-    public MultipartResolver multipartResolver() {
-        return new StandardServletMultipartResolver();
-    }
-
-}
+public class MainConfiguration {}
