5e06280c7d235d1be40e4ec9818b14d6f559b9f3
dkocheshkov <d.kocheshkov@sel-be.ru>
2025-06-06 14:30:30 +0300

    Удаляем старый код

--- src/main/java/ru/practicum/onlinestore/configuration/MainConfiguration.java
+++
@@ -1,6 +0,0 @@
-package ru.practicum.onlinestore.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class MainConfiguration {}
--- src/main/java/ru/practicum/onlinestore/dto/BuyItem.java
+++
@@ -1,3 +0,0 @@
-package ru.practicum.onlinestore.dto;
-
-public record BuyItem(long id, java.math.BigDecimal price, java.math.BigDecimal count) {}
